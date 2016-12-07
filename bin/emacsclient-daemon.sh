257e6bb90a64425bb05cfafb08940c061833a6d2
Andrea Richiardi <a.richiardi.work@gmail.com>
2016-12-07 14:08:17 -0800

    Make emacsclient-daemon.sh smarter

--- bin/emacsclient-daemon.sh
+++ bin/emacsclient-daemon.sh
@@ -3,6 +3,20 @@
 # Emacs is started as daemon in /etc/systemd/user
 #
 # See https://bugs.launchpad.net/elementaryos/+bug/1355274
+#
+# It accepts the -c/-t (defaulting to -c) parameter.
+
+do_usage() {
+    echo -n
+    echo "Usage:"
+    echo "$0 [-c|-t]"
+    exit 1
+}
+
+if [[ $# -gt 1 || "$1" != "-c" && "$1" != "-t" ]] ; then
+    do_usage
+fi
+
 XLIB_SKIP_ARGB_VISUALS=1
 
 EMACS_ICON=/usr/share/icons/hicolor/scalable/apps/emacs24.svg
@@ -21,4 +35,4 @@ fi
 # https://wiki.archlinux.org/index.php/Systemd/User$
 systemctl --user import-environment PATH SSH_ENV LANG EDITOR VISUAL ALTERNATE_EDITOR GPGKEY MANPATH NODE_PATH BOOT_JVM_OPTIONS BOOT_COLOR
 
-emacsclient -c -a emacs
+emacsclient $1 -a emacs
