1a8abd9a2653aeba2485247d519ec69107e17641
Andrea Richiardi <a.richiardi.work@gmail.com>
2016-12-16 18:49:20 -0800

    Make emacs run as normal --daemon again

--- bin/emacsclient-daemon.sh
+++ bin/emacsclient-daemon.sh
@@ -15,16 +15,15 @@ do_usage() {
 
 XLIB_SKIP_ARGB_VISUALS=1
 
+EMACS_BIN=$(which emacs)
 EMACS_ICON=/usr/share/icons/hicolor/scalable/apps/emacs24.svg
-if [ ! "active" = $(systemctl --user is-active emacs.service) ]
+
+if [ 0 -eq $(pgrep --exact --count emacs) ]
 then
     echo "Starting server."
     notify-send -i "$EMACS_ICON" -u normal -h int:transient:1 "Starting server" "Please wait."
-    systemctl --user start emacs.service
-    # kap - it is a systemd service now
-    # emacsclient -t --alternate-editor="" "$@"
 
-    while [ ! "active" = $(systemctl --user is-active emacs.service) ] ; do sleep 1 ; done
+    $EMACS_BIN --daemon
 fi
 
 EMACS_PARAMS=""
@@ -38,9 +37,5 @@ elif [ -e "$1" ] ; then
 else
     do_usage
 fi
-
-# Import vars in systemd user services$
-# https://wiki.archlinux.org/index.php/Systemd/User$
-systemctl --user import-environment PATH SSH_ENV LANG EDITOR VISUAL ALTERNATE_EDITOR GPGKEY MANPATH NODE_PATH BOOT_JVM_OPTIONS BOOT_COLOR
 
 emacsclient $EMACS_PARAMS -a emacs $EMACS_FILE
