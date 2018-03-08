926fe0297dde8edaeed8f869ea0aab82dcc4aed4
Andrea Richiardi <a.richiardi.work@gmail.com>
2018-03-08 10:40:37 -0800

    Tweak xrandr scripts in order to detect screen resolutions

--- bin/hdmi1-right-of-lvds1.sh
+++ bin/hdmi1-right-of-lvds1.sh
@@ -1,15 +1,44 @@
-#!/bin/sh
-MAIN_NAME=$(xrandr | grep -E "eDP[0-9]+ connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
-OTHER_NAME=$(xrandr | grep -E "DP1-?[0-9]+ connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+#!/bin/bash
 
-XRANDR_COMMAND="xrandr --output $OTHER_NAME --mode 1680x1050 --pos 0x0 --rotate normal \
---output $MAIN_NAME --primary --mode 1920x1080 --pos 1680x0 --rotate normal \
+set -uo pipefail
+
+# This can be either an index in the xrandr size table or width x height
+external_size=${1:-"0"}
+
+main_xrandr=$(xrandr --query | grep -E 'eDP[0-9]+ connected')
+main_name=$(echo "$main_xrandr" | awk -F '[ x+]' '/\<connected\>/{print $1}')
+main_width=$(echo "$main_xrandr" | awk -F '[ x+]' '/\<connected\>/{print $4}')
+main_height=$(echo "$main_xrandr" | awk -F '[ x+]' '/\<connected\>/{print $5}')
+main_resolution=$main_width"x"$main_height
+
+# detecting external monitors
+external_name=$(xrandr --query | grep -E "DP1-?[0-9]+ connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+if [ -z "$external_name" ]; then
+    external_name=$(xrandr --query | grep -E "HDMI-?[0-9]+ connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+fi
+
+if [ -z "$external_name" ]; then
+    echo "Failed: cannot find external monitors."
+    exit 1
+fi
+
+external_position=$main_width"x0"
+main_position=0x0
+
+# The blank space, which is part of the IFS shell variable, is used by default
+# in arrays assignment. external_name will contain a space separated list that we
+# convert to array.
+declare -a external_array
+external_array=($external_name)
+
+cmd="xrandr --output ${external_array[0]} --size $external_size --pos $external_position --rotate normal \
+--output $main_name --primary --mode $main_resolution --pos $main_position --rotate normal \
 --output VIRTUAL1 --off --output VGAi-1 --off"
 
-eval $XRANDR_COMMAND
+eval $cmd
 
 if [ "$?" -ne 0 ]; then
-    echo "Failed: $XRANDR_COMMAND"
+    echo "Failed: $cmd"
 fi
 
 exit 0
